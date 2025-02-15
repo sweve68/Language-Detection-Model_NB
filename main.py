d45164075bf156881ecfbe290464a4bde7cdca93
sweve68 <imsweve@gmail.com>
2025-02-15 11:45:30 +0530

    sixth commit

--- main.py
+++ main.py
@@ -48,8 +48,9 @@ async def predict_language(text: TextInput):
     return classes[prediction[0]]
 
 
-if __name__=='__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 
 
 
